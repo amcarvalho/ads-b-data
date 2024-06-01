f12cec72465d7154e782e897480aac3aa01b994b
Alexandre Carvalho <alexandre.m.carvalho@gmail.com>
2024-06-01 21:15:32 +0100

    several changes

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t nervokid/adsbdata:latest -f Dockerfile .
+#docker-compose up --detach
+#docker cp db.json djangowebsite_web_1:/db.json
+#docker exec djangowebsite_web_1 python manage.py loaddata /db.json
