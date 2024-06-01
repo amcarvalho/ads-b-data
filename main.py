277a7b66986f4308f98b4a63fd883ac1c3ee210a
Alexandre Carvalho <alexandre.carvalho@stenn.com>
2024-06-01 20:25:48 +0100

    several changes

--- main.py
+++ main.py
@@ -19,15 +19,3 @@ if __name__ == "__main__":
                 else:
                     dm.insert_record(data, hex_code)
             time.sleep(1)
-
-
-# api = APIClient('dummy_url')
-# data = api.fetch_test_data()
-# ns = NetworkStreamer()
-
-
-# dm = DatabaseManager('adsb_password')
-# print(dm.is_there_a_recent_record('test_id2'))
-# dm.insert_record(data, 'test_id2')
-# print(ns.fetch_data())
-# print(ns.fetch_test_data())
