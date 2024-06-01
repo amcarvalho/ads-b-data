86f346335600c5c4b1d5923fcbfcc2489e83d1f1
Alexandre Carvalho <alexandre.carvalho@stenn.com>
2024-06-01 22:09:17 +0100

    several changes

--- resources/create_database_and_user.sql
+++
@@ -1,6 +0,0 @@
-create user adsb_user with password 'adsb_password';
-create database adsb_data;
-grant connect on database adsb_data to adsb_user;
-\c adsb_data;
-create schema adsb_data;
-GRANT ALL ON SCHEMA adsb_data TO adsb_user;
--- resources/db_config.sql
+++ resources/db_config.sql
@@ -1,2 +1,7 @@
+create user adsb_user with password 'adsb_password';
+CREATE DATABASE adsb_data;
+GRANT ALL PRIVILEGES ON DATABASE adsb_data TO adsb_user;
+\c adsb_data;
+create schema adsb_data;
 create table adsb_data.adsb_data(id varchar(32), timestamp timestamp, icao_type_code varchar(50), manufacturer varchar(100), mode_s varchar(50), operator_flag_code VARCHAR(50), registered_owners VARCHAR(255), registration VARCHAR(50), type VARCHAR(100), PRIMARY KEY (id, timestamp));
 create index adsb_data_id_index ON adsb_data.adsb_data (id);
